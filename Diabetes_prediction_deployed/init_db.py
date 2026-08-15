cc7458dc22289e3b166e35b3e8c9c34ee7596539
Rajeev <jrajeevreddy2006@gmail.com>
2026-08-15 17:48:39 +0530

    Update diabetes prediction project

--- Diabetes_prediction_deployed/init_db.py
+++ Diabetes_prediction_deployed/init_db.py
@@ -1,21 +1,11 @@
 import sqlite3
 
 conn = sqlite3.connect("database.db")
-
 cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-)
-""")
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS predictions(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
     pregnancies REAL,
     glucose REAL,
     bloodpressure REAL,
